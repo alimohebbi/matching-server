c5525af68954723d7596244db429ba0c9a710f6a
ali mohebbi <ali.mohebbi@usi.ch>
2022-02-03 15:49:02 +0100

    more on perfect matcher, config

--- evaluators/custom_evaluator.py
+++ evaluators/custom_evaluator.py
@@ -20,10 +20,8 @@ class CustomEvaluator(AbstractEvaluator):
         src_columns, target_columns = add_src_target_string(descriptors_fields)
         src_descriptors = set(row[src_columns])
         src_event = ' '.join(src_descriptors)
-        src_event = remove_redundant_words(src_event)
         target_descriptors = set(row[target_columns])
         target_event = ' '.join(target_descriptors)
-        target_event = remove_redundant_words(target_event)
         return CompatibleDescriptors(src_event, target_event)
 
     def assign_score(self, descriptors: CompatibleDescriptors):
--- evaluators/perfect_evaluator.py
+++ evaluators/perfect_evaluator.py
@@ -5,7 +5,7 @@ from evaluators.abstract_evaluator import AbstractEvaluator
 
 important_filed = ['text', 'content-desc', 'resource-id', 'activity']
 config = Config()
-gt_table = pd.read_csv(config.ground_truth)
+gt_table = pd.read_csv(config.ground_truth).fillna('')
 
 
 class PerfectEvaluator(AbstractEvaluator):
@@ -21,13 +21,15 @@ class PerfectEvaluator(AbstractEvaluator):
         src = app_pair.split('-')[0]
         target = app_pair.split('-')[1]
         task = app_pair.split('-')[2]
-        condition_migration = (gt_table['src'] == src) & (gt_table['target'] == target) & (gt_table['task'] == task)
-        condition_event = (gt_table['text'] == descriptors['target_text']) & (
-                    gt_table['content-desc'] == descriptors['target_content_desc']) & \
-                          (gt_table['resource-id'] == descriptors['target_id']) & (
-                                      gt_table['activity'] == descriptors['target_activity'])
-
+        src += task
+        target += task
+        condition_migration = (gt_table['src_app'] == src) & (gt_table['target_app'] == target)
         migration = gt_table[condition_migration]
+
+        condition_event = (migration['target_text'] == descriptors['target_text']) & (
+                migration['target_content_desc'] == descriptors['target_content_desc']) & \
+                          (migration['target_id'] == descriptors['target_id']) & (
+                                  migration['target_activity'] == descriptors['target_activity'])
         migration = migration[condition_event]
         if migration.shape[0] > 0:
             return 1
